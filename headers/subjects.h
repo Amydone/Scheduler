c41411cab0da62e307c414cb719bf212be06ed4b
Maxim <frame-ebp@bk.ru>
2022-03-22 20:54:57 +1000

    add fun SubjectsFree and struct changes

--- headers/subjects.h
+++ headers/subjects.h
@@ -1,18 +1,27 @@
 #ifndef SUBJECTS_H
 #define SUBJECTS_H
 
-#define SUBJ_LENGTH 30
+#define SUBJ_NAME_LEN 30
 #define EXTENSION ".sch"
 #define EXT_SIZE 5
 #define MAX_SUBJECTS 20
 
+/*  may will be better to use linked list or smt like that
+ *
+ *  struct subject {
+ *      char s_name[30];
+ *      struct subject *next;
+ *  }
+ */
+
 struct subject {
-    char *s_name[MAX_SUBJECTS];
+    char *s_name[SUBJ_NAME_LEN];
 };
 
 struct subject* SubjectsExist(void);
 void SubjectsShow(struct subject *p);
 void SubjectsNone(void);
+struct subject* SubjectsFree(struct subject *p);
 char *GetFileExtension(char *fname, size_t len);
 
 #endif
