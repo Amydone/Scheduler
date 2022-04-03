9146065fd12c939e20b81b225cdbc3f58f268bc6
Maxim <frame-ebp@bk.ru>
2022-04-03 21:57:20 +1000

    added funcs

--- headers/login.h
+++ headers/login.h
@@ -1,6 +1,14 @@
 #ifndef LOGIN_H
 #define LOGIN_H
 
-void Entrance(void);
+#define INPUTBUFF_INT_SIZE 50
+
+void Welcome(void);
+void ScreenClear(void);
+
+char *GetFormatInput(void);
+
+int Entrance(void);
+int GetScreenSize(void);
 
 #endif
