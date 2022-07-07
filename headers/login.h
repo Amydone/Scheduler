066633c9bba73a77e24650d2ab16c3910a136f6d
Maxim <frame-ebp@bk.ru>
2022-07-08 02:27:40 +1000

    defined version of scheduler, func arguments were changed

--- headers/login.h
+++ headers/login.h
@@ -1,12 +1,14 @@
 #ifndef LOGIN_H
 #define LOGIN_H
 
+#define VERSION "0.1"
+
 #define INPUTBUFF_INT_SIZE 50
 
-void Welcome(void);
+void Welcome(unsigned width);
 void ScreenClear(void);
 
-char *GetFormatInput(void);
+char *GetFormatInput(char *buffer);
 
 int Entrance(void);
 int GetScreenSize(void);
